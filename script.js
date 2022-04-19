73c21573ede9f2377d8068bfecb1477da882038f
alonsomarce <git@marceloalonso.com.ar>
2022-04-19 00:02:45 -0300

    1ra version

---
+++ script.js
@@ -0,0 +1,66 @@
+//Esto es un alert
+//alert("Hola Mundo de Javascript")
+
+/*
+todo
+esto
+es comentario
+*/
+
+var alumno = "" //creo la variable
+let nota = 9
+
+const entidad = "Gobierno ciudad"
+
+
+
+console.log( "el nombre del alumno es " + alumno + "y su nota es " + nota)
+
+//VAN
+let nombreApellido = "juan perez"
+let User = "juan carlos"
+let _usuario
+let $user
+let nombre_apellido = "Snake case"
+
+//NO VANNNNNN
+/*
+let 1ernombre
+let nombre-apellido
+let var super break this  >> pq son palabras reservadas
+*/
+
+//STRING
+let str1 = "String con comillas dobles"
+let str2 = 'String con comillas simples'
+let str3 = `backticks`
+
+let str4 = str1 + " y " + str2
+let str5 = `${str1} y ${str2}` 
+
+console.log (str4)
+console.log (str5)
+
+
+//NUMBER
+/*
+let num1 = 2
+let num2 = 3.14
+*/
+
+//INGRESO DEL VALOR POR PARTE DEL USUARIO
+let num1 = parseFloat(prompt ("Mete el primer numero"))
+let num2 = parseFloat(prompt ("Mete el segundo numero"))
+
+//Operaciones Basicas
+let suma = num1 + num2
+let resta = num1 - num2
+let mult = num1 * num2
+let div = num1 / num2
+
+console.log("La suma es :"+suma)
+console.log("La resta es :"+resta)
+console.log("La multiplicacion es :"+multiplicacion)
+console.log("La division es :"+division)
+
+document.write("<h1> Javascript </h1>" + mult)
